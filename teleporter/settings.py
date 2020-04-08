c46182afafd0f2ec1eff8391b75e6ce8b15eee9c
Scott McAndrew <scottymcandrew@outlook.com>
2020-04-08 21:58:29 +0100

    SECRET_KEY regenerated and now env variable

--- teleporter/settings.py
+++ teleporter/settings.py
@@ -23,7 +23,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0kav7edrjvt!l!3!!3yg8@_ih%7wl605i5(3fqhu!ubghya^q3'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
